c618a5b20c55e8b31070d0c4cf9071dced864fb5
chinorovner <matiasrovner@gmail.com>
2023-02-10 18:36:57 -0300

    Ejercicio 3.a

--- app.js
+++ app.js
@@ -4,16 +4,3 @@ let nombre = prompt("Ingrese su nombre por favor")
     alert("Bienvenido " + nombre)
 
 /**/
-
-/* 3.b */
-
-let valorUno = parseInt(prompt("Ingrese un valor"))
-let valorDos = parseInt(prompt("Ingrese un segundo valor"))
-let resultado = valorUno + valorDos
-
-if(valorUno){
-    valorDos
-}{
-    alert(resultado)
-}
-/**/
