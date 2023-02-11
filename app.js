093acedccaeed049d57f0694c42ffb6df768fd24
chinorovner <matiasrovner@gmail.com>
2023-02-11 13:58:06 -0300

    Ejercicio 3.f

--- app.js
+++ app.js
@@ -43,3 +43,36 @@ if(numero1 === numero2 && numero1 === numero3){
     alert("Los 3 numeros NO son iguales")
 }
 */
+
+/* 3.e (no resuelto)
+
+let numero1 = parseInt(prompt("Ingrese el primer número"))
+let numero2 = parseInt(prompt("Ingrese el segundo número"))
+let numero3 = parseInt(prompt("Ingrese el tercer número"))
+
+if(numero1 > numero2 && numero1 > numero3){
+    console.log(numero1)
+}if(numero2 > numero1 && numero2 > numero3){
+    console.log(numero2)
+}if(numero3 > numero1 && numero3 > numero2){
+    console.log(numero3)
+}
+*/
+
+/*3.f
+
+let peso = parseInt(prompt("Ingrese su peso en KG"))
+let altura = parseInt(prompt("Ingrese su altura en metros"))
+let imc = parseInt(peso / (altura^2))
+
+
+if(imc < 18.5){
+    alert("Su peso es bajo ya que tiene un IMC (Indice de Masa Corporal) de " + imc)
+}if(imc > 18.6 && imc < 24.9){
+    alert("Su peso es normal ya que tiene un IMC (Indice de Masa Corporal) de " + imc)
+}if(imc > 25 && imc < 29.9){
+    alert("Usted tiene sobrepeso ya que tiene un IMC (Indice de Masa Corporal) de " + imc)
+}if(imc > 30){
+    alert("Usted tiene obesidad ya que tiene un IMC (Indice de Masa Corporal) de " + imc)
+}
+*/
